3f9481d3ae9aee5db9b1d82b82f03e9ae6d272a7
root <root@srv531858.hstgr.cloud>
2024-05-26 07:47:45 +0000

    deploy and gpt3 integration

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
+# Supprimer la sortie des commandes npm
+
+
 source_dir="dist/"
 destination_dir="/var/www/html"
 
-echo "Building project..."
-npm i
-npm run build
-echo "Deploying project..."
+# git pull
+echo -e "\e[0;33mPulling the client...\e[0m"
+git pull > /dev/null 2>&1
+echo -e -n "\x1B[1A\x1B[0K"
+echo -e "\e[0;32mPulling the client\e[0m"
+
+# npm i (without stdout)
+echo -e "\e[0;33mInstalling dependencies...\e[0m"
+npm i > /dev/null 2>&1
+echo -e -n "\x1B[1A\x1B[0K"
+echo -e "\e[0;32mInstalling dependencies\e[0m"
+
+# npm run build (without stdout)
+echo -e "\e[0;33mBuilding Project...\e[0m"
+npm run build > /dev/null 2>&1
+echo -e -n "\x1B[1A\x1B[0K"
+echo -e "\e[0;32mBuilding Project\e[0m"
+
 
 # Supprimer le contenu du répertoire de destination s'il existe
 if [ -d "$destination_dir" ]; then
-    echo "Suppression du contenu de $destination_dir..."
+    echo -e "\e[0;33mSuppression du contenu de $destination_dir...\e[0m"
     rm -r "$destination_dir"/*
-    echo "Contenu de $destination_dir supprimé avec succès."
+    echo -e -n "\x1B[1A\x1B[0K"
+    echo -e "\e[0;32mSuppression du contenu de $destination_dir\e[0m"
 else
     echo "Le répertoire de destination $destination_dir n'existe pas."
 fi
 
-Vérifie si le répertoire source existe
+# Vérifie si le répertoire source existe
 if [ -d "$source_dir" ]; then
     # Copie récursive de tous les fichiers et répertoires du répertoire source vers le répertoire de destination
     cp -r "$source_dir"/* "$destination_dir"
--- server.py
+++ server.py
@@ -1,32 +1,64 @@
+import os
 from flask import Flask, request, jsonify
 from transformers import GPTNeoForCausalLM, AutoTokenizer, GPT2LMHeadModel, GPT2Tokenizer
 from flask_cors import CORS
+from openai import OpenAI
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# NEO GPT
-# model_name = "EleutherAI/gpt-neo-125M"
-# model = GPTNeoForCausalLM.from_pretrained(model_name)
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Initialiser le client OpenAI
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# GPT 2
-model_name = "gpt2"
-model = GPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+# Charger les modèles et les tokenizers
+model_gpt2 = GPT2LMHeadModel.from_pretrained("gpt2")
+tokenizer_gpt2 = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer_gpt2.pad_token_id = tokenizer_gpt2.eos_token_id
 
-tokenizer.pad_token_id = tokenizer.eos_token_id
+model_gpt_neo = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
+tokenizer_gpt_neo = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+tokenizer_gpt_neo.pad_token_id = tokenizer_gpt_neo.eos_token_id
 
 @app.route('/generate', methods=['POST'])
 def generate_text():
     input_data = request.json
     input_text = input_data.get('input_text', '')
+    selected_model = input_data.get('model', '')
 
     if not input_text:
         return jsonify({'error': 'No input text provided'}), 400
 
-    inputs = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
+    if selected_model == 'gpt2':
+        model = model_gpt2
+        tokenizer = tokenizer_gpt2
+    elif selected_model == 'gpt-neo':
+        model = model_gpt_neo
+        tokenizer = tokenizer_gpt_neo
+    elif selected_model == 'gpt3':
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": input_text,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        str_res = str(response)
+        start_index = str_res.find("content=") + len("content=\"")
 
+        if (str_res[start_index - 1] == '"'):
+            end_index = str_res.find("\"", start_index)
+            content = str_res[start_index:end_index]
+        else:
+            end_index = str_res.find("\'", start_index)
+            content = str_res[start_index:end_index]
+        # print(content)
+        return jsonify({'generated_text': content})
+
+    inputs = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
     outputs = model.generate(
         inputs.input_ids,
         attention_mask=inputs.attention_mask,
